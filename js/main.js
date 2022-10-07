2474034168068f1a64f2a760b2e7931b5787adcb
Nathalya-Amorim <97710266+Nathalya-Amorim@users.noreply.github.com>
2022-10-07 19:58:06 -0400

    adding button functionality with javaScript

---
+++ js/main.js
@@ -0,0 +1,52 @@
+import { getData } from "./modules/dataMiner.js";
+
+(() => {
+
+let theTemplate = document.querySelector("#things-template").content,
+    theThings = document.querySelector(".things-section"),
+    buttonContainer = document.querySelector('.query-controls'),
+    activityList;
+
+    // Adds 
+    function addButtons(data) { 
+        //console.log(data.things); 
+        let container = new DocumentFragment(); 
+        activityList = data.things;
+        data.things.forEach(thing => { 
+            let buttonEl = document.createElement('button'); 
+            buttonEl.className = 'catButton'; 
+            buttonEl.dataset.thing = thing.activity.id;  
+            buttonEl.textContent = thing.activity.name; 
+
+            buttonEl.addEventListener('click', buttonEvent) 
+            container.appendChild(buttonEl); 
+        });
+
+        buttonContainer.appendChild(container); 
+
+    }
+
+    function buttonEvent(){ 
+        theThings.textContent = ''; 
+        activityList.forEach(thing => { 
+            if(thing.activity.id == this.dataset.thing) {
+                let panel = theTemplate.cloneNode(true), 
+                containers = panel.firstElementChild.children; 
+
+                containers[0].querySelector('img').src = 'images/' + thing.activity.image; 
+                containers[1].textContent = thing.activity.name;
+                containers[2].textContent = thing.activity.description;
+
+                theThings.appendChild(panel); 
+            }
+        });
+    }
+
+
+    
+    getData("./data.json", addButtons);
+
+})();
+
+
+
